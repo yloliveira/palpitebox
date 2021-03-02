9ca58c62c9cbb25c374b20400272643c6a690bac
Yan Oliveira <oliveira.yan02@gmail.com>
2021-03-02 10:39:23 -0300

    feat: add header and css module

---
+++ components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const Header = () => (
+  <div className={styles.wrapper}>
+    <p>PalpiteBox</p>
+  </div>
+);
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import '../styles/global.css';
 import { AppProps } from 'next/app';
+import styles from '../styles/app.module.css';
+import Header from '../components/Header';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+
+    <div className={styles.wrapper}>
+      <Header />
+      <div className={styles.content}>
+        <Component {...pageProps} />
+      </div>
+    </div>
+
+  );
 }
 
 export default App;
